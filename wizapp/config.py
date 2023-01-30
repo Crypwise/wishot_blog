743a5d64bf2cdd68465181fdbcc593e9fc5527e5
Alom Wisdom <103900220+Wishot-Cipher@users.noreply.github.com>
2023-01-30 02:35:15 +0100

    Update config.py

--- wizapp/config.py
+++ wizapp/config.py
@@ -3,7 +3,7 @@ import os
 
 class config:
     SECRET_KEY = '78aea450efb53c77db6c2564c55ed647'
-    SQLALCHEMY_DATABASE_URI = os.environ('SQLALCHEMY_DATABASE_URI_BLOG')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_BLOG')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
